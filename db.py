8d418f2ff104cf83236fa512eef175fb34a66fd2
Quyên <11520320@gm.uit.edu.vn>
2025-12-19 21:54:37 +0700

    Update db.py

--- db.py
+++ db.py
@@ -1,5 +1,5 @@
 from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker, AsyncSession
-from .config import DATABASE_URL
+from config import DATABASE_URL
 
 engine = create_async_engine(DATABASE_URL, pool_pre_ping=True)
 SessionLocal = async_sessionmaker(engine, expire_on_commit=False, class_=AsyncSession)
